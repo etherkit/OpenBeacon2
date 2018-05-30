93e815deeba64eefc47c980f8ecbc08d2a384517
Jason Milldrum <milldrum@gmail.com>
2018-05-29 18:08:29 -0700

    Update timesync.py to ensure Win compatibility

--- client/timesync.py
+++ client/timesync.py
@@ -3,13 +3,18 @@
 import serial
 import serial.tools.list_ports
 import sys
+# import tty
+# import termios
 import time
+#import _thread
+import threading
 from timeit import default_timer as timer
 import argparse
 
 # Arduino serial dev paramaters
 if sys.platform.startswith('win'):
     DEVICE = 'COM1'
+
 elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
     DEVICE = '/dev/ttyACM0'
 elif sys.platform.startswith('darwin'):
@@ -20,24 +25,64 @@ else:
 
 BAUD = 57600
 
+
 class ListSerialPorts(argparse.Action):
-    def __init__(self, option_strings, dest, nargs = 0, **kwargs):
-        super(ListSerialPorts, self).__init__(option_strings, dest, nargs = nargs, **kwargs)
+    def __init__(self, option_strings, dest, nargs=0, **kwargs):
+        super(ListSerialPorts, self).__init__(
+            option_strings, dest, nargs=nargs, **kwargs)
+
     def __call__(self, parser, namespace, values, option_string=None):
         print("Available serial ports:")
         for port in serial.tools.list_ports.comports():
             print(str(port.device))
         sys.exit(0)
 
+
 # Set up argument parser
-arg_parser = argparse.ArgumentParser(description = "OpenBeacon Mini Synchronization")
-arg_parser.add_argument("--port", "-p", help = "Serial port connected to OpenBeacon Mini", nargs = '?', default = DEVICE)
-arg_parser.add_argument("--baud", "-b", help = "Baud rate of the serial connection", nargs = '?', default = BAUD)
-arg_parser.add_argument("--list-ports", "-l", help = "Enumerate available serial ports", nargs = 0, action = ListSerialPorts)
-arg_parser.add_argument("--verbosity", "-v", action="store_true", help="Increase output verbosity")
+arg_parser = argparse.ArgumentParser(
+    description="OpenBeacon Mini Synchronization",
+    epilog="Press Escape or Q to quit")
+arg_parser.add_argument(
+    "--port", "-p", help="Serial port connected to OpenBeacon Mini", nargs='?', default=DEVICE)
+arg_parser.add_argument(
+    "--baud", "-b", help="Baud rate of the serial connection", nargs='?', default=BAUD)
+arg_parser.add_argument(
+    "--list-ports", "-l", help="Enumerate available serial ports", nargs=0, action=ListSerialPorts)
+arg_parser.add_argument("--verbosity", "-v",
+                        action="store_true", help="Increase output verbosity")
 args = arg_parser.parse_args()
 
+# Set up non-blocking keyboard read
+try:
+    from msvcrt import getch  # try to import Windows version
+except ImportError:
+    import tty, termios
+    def getch():   # define non-Windows version
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        try:
+            tty.setraw(sys.stdin.fileno())
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        return ch
+
+
+def keypress():
+    global char
+    char = getch()
+
+
+# Start non-blocking keypress thread
+t = threading.Thread(target=keypress)
+t.daemon = True
+t.start()
+
+
 def main():
+    global char
+    char = None
+
     # Open serial port
     try:
         ser = serial.Serial(port=args.port, baudrate=args.baud,
@@ -46,19 +91,16 @@ def main():
         print('Cannot open serial port ' + args.port)
         sys.exit(0)
 
-
     # Wait for ASCII bell, then send the Unix time string
     while True:
         ser_in = ser.read()
         if('\a' in ser_in.decode()):
-            # ser.reset_input_buffer()
             cur_time = int(time.time())
             while(cur_time == int(time.time())):
                 pass
-
             time_str = "T" + str(int(time.time()))
             ser.write(time_str.encode())
-            print('Time sync at ' + time.asctime(time.gmtime()))
+            print("Time sync at " + time.asctime(time.gmtime()) + "\r")
         elif('\f' in ser_in.decode()):
             start = timer()
             start_time = time.strftime("%H:%M:%S - ", time.gmtime())
@@ -70,6 +112,11 @@ def main():
         elif('\v' in ser_in.decode()):
             if args.verbosity == 2:
                 print(ser.readline())
+
+        if char is not None:
+            if char == b'q' or char == b'\x1b':  # x1b is ESC
+                sys.exit(0)
+            char = None
 
 
 if __name__ == "__main__":
