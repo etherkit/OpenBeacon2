49dfec7b83de6129aabf8c7fc8a743d83c9eb6d2
Jason Milldrum <milldrum@gmail.com>
2019-09-17 17:24:41 -0700

    Generate ob2sync for Windows

---
+++ client/win/venv/Lib/site-packages/cmd2/clipboard.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+"""
+This module provides basic ability to copy from and paste to the clipboard/pastebuffer.
+"""
+import pyperclip
+# noinspection PyProtectedMember
+from pyperclip import PyperclipException
+
+# Can we access the clipboard?  Should always be true on Windows and Mac, but only sometimes on Linux
+try:
+    # Try getting the contents of the clipboard
+    _ = pyperclip.paste()
+except PyperclipException:
+    can_clip = False
+else:
+    can_clip = True
+
+
+def get_paste_buffer() -> str:
+    """Get the contents of the clipboard / paste buffer.
+
+    :return: contents of the clipboard
+    """
+    pb_str = pyperclip.paste()
+    return pb_str
+
+
+def write_to_paste_buffer(txt: str) -> None:
+    """Copy text to the clipboard / paste buffer.
+
+    :param txt: text to copy to the clipboard
+    """
+    pyperclip.copy(txt)
