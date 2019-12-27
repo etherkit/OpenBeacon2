f53f2f0290b983c6cacbcab9ae0eb04960fa5569
Jason Milldrum <milldrum@gmail.com>
2019-12-27 15:50:25 -0800

    Add single_dfcw_wspr.py script

---
+++ client/examples/single_dfcw_wspr.py
@@ -0,0 +1,68 @@
+"""Single Band DFCW and WSPR Script
+
+This script will transmit buffer 1 in DFCW6 mode on a fixed frequency 
+within a band and then immediately transmit WSPR on the same band on
+a random frequency within the WSPR sub-band.
+"""
+
+tx_over = False
+
+# Edit these values for your desired DFCW transmission frequencies
+dfcw_freq_table = {"80m": 3560000,
+                   "60m": 5358000,
+                   "40m": 7039800,
+                   "30m": 10139900,
+                   "20m": 14096800,
+                   "17m": 18080000,
+                   "15m": 21060000,
+                   "12m": 24926000,
+                   "10m": 28060000}
+
+callsign = 'N0CALL'
+band = '30m'  # Change the desired band here
+buffer = 'N0CALL HNY'  # Change the message here
+
+def TXEnd():
+    global tx_over
+
+    tx_over = True
+
+
+# Register callback for TX End
+self.register('tx_end', TXEnd)
+
+self.async_alert("Script begin - Ctrl-C to terminate")
+
+try:
+    app('set callsign {}'.format(callsign))
+    app('set msg_buffer_1 \"{}\"'.format(buffer))
+    app('set buffer 1')
+    app('set band {}'.format(band))
+
+    while True:
+        # DFCW6 transmission
+        app('set mode dfcw6')
+        app('set rnd_tx false')
+        app('set base_freq {}'.format(dfcw_freq_table[band]))
+        app('tx enable')
+        while tx_over == False:
+            pass
+        tx_over = False
+        app('tx disable')
+
+        # WSPR transmission
+        app('set mode wspr')
+        app('set rnd_tx true')
+        app('tx enable')
+        while tx_over == False:
+            pass
+        tx_over = False
+        app('tx disable')
+
+except:
+    app('tx cancel')
+    app('tx disable')
+    self.async_alert("Script termniation")
+
+app('tx disable')
+self.async_alert("Script end")
